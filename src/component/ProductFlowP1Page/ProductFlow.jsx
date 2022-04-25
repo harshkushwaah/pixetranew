5aebb00fb671e79ded516d84571dd172422376c3
Harsh Kushwah <harshkingkushwah@gmail.com>
2022-04-25 11:33:55 +0530

    animated the product page scroll element (harsh)

--- src/component/ProductFlowP1Page/ProductFlow.jsx
+++ src/component/ProductFlowP1Page/ProductFlow.jsx
@@ -9,8 +9,8 @@ import ReviewSlider from "../Sliders/ReviewSlider"
 import Btn from "./Btn";
 import close from "./img/X.png"
 import { useNavigate}from 'react-router-dom';
-import React from 'react';
-
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 // function Scroll(){
 //     return{
 //         document.getElementById("target-id").style.display = "none";
@@ -18,6 +18,28 @@ import React from 'react';
 // }
 function ProductFlow() {
     const navigate = useNavigate();
+
+
+    const [lastYPos, setLastYPos] = React.useState(0);
+    const [shouldShowActions, setShouldShowActions] = React.useState(false);
+  
+    React.useEffect(() => {
+      function handleScroll() {
+        const yPos = window.scrollY;
+        const isScrollingUp = yPos < lastYPos;
+  
+        setShouldShowActions(isScrollingUp);
+        setLastYPos(yPos);
+      }
+  
+      window.addEventListener("scroll", handleScroll, false);
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll, false);
+      };
+    }, [lastYPos]);
+
+
     return (
     <div className='product-flow'>
         <div className='product-upper'>
@@ -45,11 +67,13 @@ function ProductFlow() {
             <p className='review'>Reviews</p>
             <ReviewSlider />
         </div>
-                <div className='scroll'>
+                <motion.div  initial={{ opacity: 0 }}
+      animate={{ opacity: shouldShowActions ? 1 : 0 }}
+      transition={{ opacity: { duration: 0.2 } }} className='scroll'>
                 <span>^^</span>
                 <p>Scroll down for more info</p>
                 <span>^^</span>
-            </div>
+            </motion.div>
             <div className='wantBtn-Box'>
             <Btn text="Want?" emoji="🤩" />
             </div>
